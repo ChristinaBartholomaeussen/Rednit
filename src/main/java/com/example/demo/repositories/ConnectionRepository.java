66bf1c593e97ef4d5a291fae4582031fbf06637f
GenuinelyMeizner <frederikmpetersen@gmail.com>
2020-11-05 16:01:46 +0100

    Ændret i sout i ConnectionRepository

--- src/main/java/com/example/demo/repositories/ConnectionRepository.java
+++ src/main/java/com/example/demo/repositories/ConnectionRepository.java
@@ -8,7 +8,7 @@ public class ConnectionRepository {
         Connection connection = null;
         try {
              connection = DriverManager.getConnection("jdbc:mysql://den1.mysql3.gear.host:3306/Rednit", "rednit", "tinder!");
-             System.out.println("Connection Ok");
+             System.out.println("Connection OK");
         } catch (SQLException e) {
             System.out.println("No Connection");
         }
