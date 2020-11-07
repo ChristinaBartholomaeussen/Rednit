004bd4c1a5e055abe6ac1a7650971017286b4f5a
christina bartholomæussen <chri782z@edu.ucl.dk>
2020-11-07 15:09:19 +0100

    ...

--- src/main/java/com/example/demo/controllers/HomeControlle.java
+++ src/main/java/com/example/demo/controllers/HomeControlle.java
@@ -7,6 +7,7 @@ import com.example.demo.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
@@ -17,12 +18,16 @@ public class HomeControlle {
 
     //TODO Loginmapping bør ligge i LoginController - Home skal kun indeholde hvad vi viser i "Udforsk"
     //TODO "Udforsk" efter filnavnet er unødvendigt, det ser lidt rodet ud?
+
+    //TODO indsæt metode, der tjekker om man er logget ind og hvis man ikke er, så skal redirecte til loginsiden
 //    @GetMapping("/loginpage")
 //    public String login(Model userModel){
 //
 //        userModel.addAttribute("userToDisplay", userToDisplay);
 //        return "loginPage";
 //    }
+
+
 
     @PostMapping("/postLoginpage")
     public boolean userLogin(WebRequest dataFromForm){
