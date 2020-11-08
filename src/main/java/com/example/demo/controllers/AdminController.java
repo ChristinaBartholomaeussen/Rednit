d017f6794b223760bcaac4fb615ad6c1e3c4a5e0
Rune Petersen <runembpetersen@gmail.com>
2020-11-08 17:54:12 +0100

    List af brugere til frontend adminPage.html

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -4,10 +4,10 @@ import com.example.demo.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,28 +15,30 @@ import java.util.List;
 @Controller
 public class AdminController
 {
-    @GetMapping("/admin")
-    public String index(Model adminModel)
-    {
-        List<User> users = new ArrayList<>();
-        Date date = new Date();
+    List<User> allUsers = new ArrayList<>();
 
+    @GetMapping("/admin")
+    public String adminPage(Model adminModel)
+    {
+        //Dummy code - 45 nye Users
         for(int i = 0; i < 45; i++)
         {
-            users.add(new User("email"+i,"password"+i,"firstName"+i,"lastName"+i,date,i,i,"bio"+i));
+            Date date = new Date(i);
+            allUsers.add(new User("email"+i,"password"+i,"firstName"+i,"lastName"+i,date,i,i,"bio"+i));
         }
 
-        adminModel.addAttribute("users", users);
+        User user = new User();
+
+        adminModel.addAttribute("users", allUsers);
+        adminModel.addAttribute("user",user);
         adminModel.addAttribute("adminModel", adminModel);
-
-
         return "adminPage";
     }
 
-    @PostMapping("/postAdmin")
-    public String index(WebRequest webRequest)
+    @PostMapping("/admin")
+    public String adminPageDelete(@ModelAttribute User user, Model model)
     {
-
+        model.addAttribute("user", "user");
         return "adminPage";
     }
 }
