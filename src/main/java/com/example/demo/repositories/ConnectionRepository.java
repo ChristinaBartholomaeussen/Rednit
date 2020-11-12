b25d93d13cf63bf531dd560605fd560472b4bbb9
GenuinelyMeizner <frederikmpetersen@gmail.com>
2020-11-12 15:52:40 +0100

    Fjernet "Connection OK"

--- src/main/java/com/example/demo/repositories/ConnectionRepository.java
+++ src/main/java/com/example/demo/repositories/ConnectionRepository.java
@@ -10,7 +10,6 @@ public class ConnectionRepository {
         Connection connection = null;
         try {
              connection = DriverManager.getConnection("jdbc:mysql://den1.mysql3.gear.host:3306/Rednit", "rednit", "tinder!");
-             System.out.println("Connection OK");
         } catch (SQLException e) {
             System.out.println("No Connection");
         }
