d1223045f3d81ae287d3cce99a360f14a15d51b4
runembp <runembpetersen@gmai.com>
2020-11-13 11:52:16 +0100

    Oprydning i whitespaces / lavpraktiske ting

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -14,24 +14,12 @@ public class HomeController {
         return "loginPage";
     }
 
-@GetMapping("/home")
+    @GetMapping("/home")
     public String home(){
-    return "explore";
-}
-
-
-    //TODO Loginmapping bør ligge i LoginController - Home skal kun indeholde hvad vi viser i "Udforsk"
-    //TODO "Udforsk" efter filnavnet er unødvendigt, det ser lidt rodet ud?
-
-    //TODO indsæt metode, der tjekker om man er logget ind og hvis man ikke er, så skal redirecte til loginsiden
-
-
-
-
-
-
-
+        return "explore";
     }
+
+}
 
 
 
