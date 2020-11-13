ba7cb74d52243b2eefdd116a0bec0394e495b50b
christina bartholomæussen <chri782z@edu.ucl.dk>
2020-11-13 16:59:49 +0100

    slettet metode i match service

--- src/main/java/com/example/demo/services/MatchService.java
+++ src/main/java/com/example/demo/services/MatchService.java
@@ -1,15 +1,15 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Match;
-import com.example.demo.models.User;
-import com.example.demo.repositories.MatchRepository;
-import com.example.demo.repositories.UserRepository;
 
-import java.util.ArrayList;
+import com.example.demo.repositories.MatchRepository;
+
+
+
 import java.util.List;
 
 public class MatchService {
-	ArrayList<Match> matchList = new ArrayList<Match>();
+
     MatchRepository matchRepository = new MatchRepository();
 
     public void insertPotentialMatch(int userFrom, int userTo) {
